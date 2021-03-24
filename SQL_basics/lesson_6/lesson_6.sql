ad9956121c9014f5ae5ffc4eca2a23a59ddfa1a5
byteturner <dmtokwd@gmail.com>
2021-03-24 16:40:20 +0300

    edit lesson 6

--- SQL_basics/lesson_6/lesson_6.sql
+++ SQL_basics/lesson_6/lesson_6.sql
@@ -30,3 +30,16 @@ from likes l
 group by age
 order by age
 limit 10;
+
+# Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети
+select p2.first_name as      name,
+       p2.last_name  as      lastname,
+       count(l.id)   as      total_likes,
+       count(m.from_user_id) total_messages
+from likes l
+         join users u on l.user_id = u.id
+         join profiles p2 on p2.user_id = u.id
+         join messages m on u.id = m.from_user_id
+group by u.id
+order by total_likes, total_messages
+limit 10;
